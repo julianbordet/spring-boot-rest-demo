1c6da31280faf331de9ff22fd77ac9233d42a931
Julian <bordet.julian@gmail.com>
2020-09-29 10:39:38 -0300

    Initial project setup

---
+++ src/test/java/cc/jbdev/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.jbdev.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
